352155f9cce5229eccf5d46a38ed592c2cb43e15
baishun <baishun@wodcloud.com>
2023-01-09 10:17:17 +0800

    add: hack csdn more btn

---
+++ hack csdn more btn.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         hack csdn more btn
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  令人讨厌的CSDN查看更多按钮，破解，自动点击。
+// @author       You
+// @match        https://blog.csdn.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=csdn.net
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  const intervalDelay = 100; // ms
+  const btnClassName = ".hide-preCode-bt"; // 可能未来会发生变化，不定期调整！
+  let maxTimeout = 3000; // ms
+  let duration = 0;
+  let timer = setInterval(() => {
+    duration += intervalDelay;
+    let fuckTheBtn = document.querySelectorAll(btnClassName);
+    if (fuckTheBtn.length) {
+      fuckTheBtn.forEach((el) => {
+        el.click();
+      });
+      clearInterval(timer);
+    }
+    if (duration > maxTimeout) {
+      // 超时强制清除计时器
+      clearInterval(timer);
+    }
+  }, intervalDelay);
+})();
